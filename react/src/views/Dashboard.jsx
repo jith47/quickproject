0ad9c1d54d0d472b0f386f1fa83fc3577574903b
jithin_ecaret <jithin@ecaret.com>
2023-06-20 22:35:38 +0530

    dsh

--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -1,7 +1,26 @@
 export default function Dashboard() {
     return (
         <div>
-            Dashboard
+          {/* Header */}
+          <header>
+            <h1>Dashboard</h1>
+          </header>
+    
+          {/* Sidebar */}
+          <aside>
+            <ul>
+              <li>Dashboard</li>
+              <li>Users</li>
+              <li>Products</li>
+              <li>Settings</li>
+            </ul>
+          </aside>
+    
+          {/* Main Content */}
+          <main>
+            <h2>Welcome to the Dashboard</h2>
+            {/* Add your dashboard content here */}
+          </main>
         </div>
-    )
+      );
 }
